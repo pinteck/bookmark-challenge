9d56255263deab141d11151f7300e04de401766a
pinar <pnr_ozbakir@msn.com>
2021-10-13 12:49:14 +0100

    09: Set up test environment

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,10 +2,17 @@ require 'bookmark'
 
 describe '.all' do
   it 'shows the bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.twitter.com/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
     bookmarks = Bookmark.all
 
-    expect(bookmarks).to include('http://www.google.com')
-    expect(bookmarks).to include('http://www.makersacademy.com/')
+    expect(bookmarks).to include('http://www.makersacademy.com')
     expect(bookmarks).to include('http://www.twitter.com/')
+    expect(bookmarks).to include('http://www.google.com')
   end
 end
